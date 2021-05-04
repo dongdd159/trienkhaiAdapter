3e837472dcb2149e7af4cca40008b3874a1453c6
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-05-05 00:13:29 +0700

    trienkhaiAdapter

---
+++ src/Client.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Client {
+    private FileCalculator fileCalculator;
+
+    public Client(FileCalculator fileCalculator) {
+        this.fileCalculator = fileCalculator;
+    }
+
+    public void printFileSize(String path){
+        long size = fileCalculator.calculateSize(path);
+        System.out.println("Size: " + size);
+    }
+
+}
